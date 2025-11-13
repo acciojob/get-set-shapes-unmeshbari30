38e0ccd7e0a1a7fd5e932482bcfcfed8ec7a396d
Unmesh Bari <unmeshbari5@gmail.com>
2025-11-13 22:34:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,33 @@
 //complete this code
-class Rectangle {}
+class Rectangle {
+	constructor(width, height){
+		this._width = width;
+		this._height = height;
+	}
 
-class Square extends Animal {}
+	get width(){
+		return this._width;
+	}
+	get height(){
+		return this._height;
+	}
+
+	getArea(){
+		let area = this._height * this._width;
+		return area;
+	}
+}
+
+class Square extends Animal {
+	constructor(side){
+		super(side, side)
+	}
+	
+	getPerimeter(){
+		let per = 2 * (super.height + super.width)
+		return per;		
+	}
+}
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
